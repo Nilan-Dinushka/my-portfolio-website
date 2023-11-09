2ce256558aaa5b4567a50935b6adfc5f5e03fa2a
Nilan Dinushka <nilandinushka5@gmail.com>
2023-11-10 01:12:29 +0530

    Complete email service

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,8 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +18,44 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value} = e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+ 
+    emailjs.send('service_tkr9p1p', 'template_6zdsn2p', 
+    {
+      from_name: form.name,
+      to_name: 'Nilan',
+      from_email: form.email,
+      to_email: 'nilandinushka5@gmail.com',
+      message: form.message,
+    },
+    'wlH9Uby6dsqg0mF_W'
+    )
+    .then(() => {
+      setLoading(false);
+      alert('Thank you. I will get back to you as soon as possible.');
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+
+      console.log(error);
+
+      alert('Something went wrong.')
+    })
+  }
 
   return (
     <div className="xl:my-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
