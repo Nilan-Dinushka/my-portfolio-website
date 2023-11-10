eb45ee226865532dcb6756423ab1db4fd754a09b
Nilan Dinushka <nilandinushka5@gmail.com>
2023-11-10 09:47:11 +0530

    Edit hero section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,7 +11,7 @@ const Computers = ({ isMobile }) => {
       <hemisphereLight intensity={4} groundColor="black" />
       <pointLight intensity={1} />
       <spotLight position={[-20,50,10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024}/>
-      <primitive object={computer.scene} scale={isMobile ? 0.7 : 0.75} position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]}/>
+      <primitive object={computer.scene} scale={isMobile ? 0.5 : 0.6} position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]}/>
     </mesh>
   );
 };
